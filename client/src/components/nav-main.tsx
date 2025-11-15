b5ca8b036ad4e66635fa3a6ea1476c3ea1e1ea47
Bohdan Chokhlenko <105508224+den-ffv@users.noreply.github.com>
2025-11-15 23:28:23 +0200

    refactor: update sidebar component structure and improve navigation items

--- client/src/components/nav-main.tsx
+++ client/src/components/nav-main.tsx
@@ -1,7 +1,6 @@
-import { ChevronRight, type LucideIcon } from "lucide-react"
+import { type LucideIcon } from "lucide-react"
 
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "@/components/ui/collapsible"
-import { SidebarGroup, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarMenuSub, SidebarMenuSubButton, SidebarMenuSubItem } from "@/components/ui/sidebar"
+import { SidebarGroup, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
 import { NavLink } from "react-router-dom"
 
 type NavMainProps = {
@@ -9,11 +8,6 @@ type NavMainProps = {
     title: string
     url: string
     icon?: LucideIcon
-    isActive?: boolean
-    items?: {
-      title: string
-      url: string
-    }[]
   }[]
 }
 
@@ -23,45 +17,16 @@ export function NavMain({ items }: NavMainProps) {
       {/* <SidebarGroupLabel>Platform</SidebarGroupLabel> */}
       <SidebarMenu>
         {items.map((item, index) => (
-          item.items ? (
-            <Collapsible key={index} asChild defaultOpen={item.isActive} className="group/collapsible">
-              <SidebarMenuItem>
-                <CollapsibleTrigger asChild>
-                  <SidebarMenuButton tooltip={item.title}>
-                    {item.icon && <item.icon />}
-                    <span>{item.title}</span>
-                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                  </SidebarMenuButton>
-                </CollapsibleTrigger>
-                <CollapsibleContent>
-                  <SidebarMenuSub>
-                    {item.items?.map((subItem, index) => (
-                      <SidebarMenuSubItem key={index}>
-                        <SidebarMenuSubButton asChild>
-                          <a href={subItem.url}>
-                            <span>{subItem.title}</span>
-                          </a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                </CollapsibleContent>
-              </SidebarMenuItem>
-            </Collapsible>
-          ): (
-            <NavLink key={index} to={item.url}>
-              <Collapsible asChild defaultOpen={item.isActive} className="group/collapsible">
-                <SidebarMenuItem>
-                  <CollapsibleTrigger asChild>
-                    <SidebarMenuButton tooltip={item.title}>
-                      {item.icon && <item.icon />}
-                      <span>{item.title}</span>
-                    </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                </SidebarMenuItem>
-              </Collapsible>
+          <SidebarMenuItem key={index}>
+            <NavLink to={item.url}>
+              {({ isActive }) => (
+                <SidebarMenuButton tooltip={item.title} data-active={isActive}>
+                  {item.icon && <item.icon />}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              )}
             </NavLink>
-          )
+          </SidebarMenuItem>
         ))}
       </SidebarMenu>
     </SidebarGroup>
